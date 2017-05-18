79b36f7e27ec669b38210c4a264b34c77769aea4
aizdes2015 <aizdes2015@gmail.com>
2017-05-18 18:52:22 +0300

    Added enum extensions

---
+++ SaaUtils/EnumUtil.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+namespace SaaUtils
+{
+	/// <summary>
+	/// Enum extensions
+	/// </summary>
+	public static class EnumUtil
+	{
+
+		/// <summary>
+		/// Last error message
+		/// </summary>
+		public static string LastError { get; private set; } = default(string);
+
+		/// <summary>
+		/// Returns attribute value
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="e"></param>
+		/// <returns></returns>
+		private static T GetAttribute<T>(this Enum e)where T : Attribute
+		{
+			try
+			{
+				Type t = typeof(T), et = e.GetType();
+				FieldInfo fi = et.GetField(Enum.GetName(et, e));
+				if (fi != null && Attribute.IsDefined(fi, t))
+					return Attribute.GetCustomAttribute(fi, t) as T;
+			}
+			catch (Exception ex)
+			{
+				LastError = $"Error extracting attribute {nameof(T)}: {ex.Message}";
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Returns field name value
+		/// </summary>
+		/// <param name="e"></param>
+		/// <returns></returns>
+		public static string FieldName(this Enum e)
+		{
+			FieldNameAttribute a = e.GetAttribute<FieldNameAttribute>();
+			return a?.Value ?? string.Empty;
+		}
+
+	}
+
+}
---
+++ SaaUtils/FieldNameAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SaaUtils
+{
+	
+	/// <summary>
+	/// Field name attribute
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field)]
+	public class FieldNameAttribute : Attribute
+	{
+		/// <summary>
+		/// Default value
+		/// </summary>
+		public static FieldNameAttribute Default { get; } = new FieldNameAttribute(default(string));
+
+		/// <summary>
+		/// Field name value
+		/// </summary>
+		public string Value { get; }
+
+		/// <summary>
+		/// Field name constructor
+		/// </summary>
+		/// <param name="name"></param>
+		public FieldNameAttribute(string name)
+		{
+			Value = name;
+		}
+	}
+
+}
